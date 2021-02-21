5f7d42136a91171cbefe5a4827f3711d0ddd2e6d
yuchan choe <youcank20@gmail.com>
2021-02-21 17:16:47 +0900

    클라임 미완성

--- Kirbies/01_WinMain/Climb.h
+++ Kirbies/01_WinMain/Climb.h
@@ -3,5 +3,15 @@
 
 class Climb : public State
 {
-};
+	class Player* mPlayer;
+	class Bottom* mBottom;
 
+	float mX, mY;
+
+	
+
+public:
+	void Init()override;
+	void Update()override;
+	void Render(HDC hdc)override;
+};
