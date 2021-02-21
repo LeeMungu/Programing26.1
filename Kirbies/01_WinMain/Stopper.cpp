b27a5a0e6bc378047a5ac4880c38d0b520920ef3
corea_89@naver.com <corea_89@naver.com>
2021-02-21 14:00:14 +0900

    스토퍼 랜더추가

--- Kirbies/01_WinMain/Stopper.cpp
+++ Kirbies/01_WinMain/Stopper.cpp
@@ -30,11 +30,10 @@ void Stopper::Update()
 	if (mPlayer->GetSpeed() != 0)
 	{
 		mPlayer->SetSpeed(0);
-		mRect = mPlayer->GetRect();
 		StopperObject* stopperObject = new StopperObject(mPlayer->GetX(),mPlayer->GetY(), mPlayer->GetSizeX(), mPlayer->GetSizeY());
+		stopperObject->Init();
 		ObjectManager::GetInstance()->AddObject(ObjectLayer::StopperObject, stopperObject);
 		mCurrentAnimation = mStopperAnimation;
-
 	}
 
 }
@@ -45,4 +44,13 @@ void Stopper::Play()
 
 void Stopper::Stop()
 {
+}
+
+void Stopper::Render(HDC hdc)
+{	
+	ObjectManager::GetInstance()->Render(hdc);
+	if (ObjectManager::GetInstance()->FindObject("Stopper") != nullptr)
+	{
+		mImage->FrameRender(hdc, mPlayer->GetX(), mPlayer->GetY(), 0, 0);
+	}
 }
