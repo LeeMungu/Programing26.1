d436d080f1a7e7144ee99691f710a1ffdadeaae6
eshilia <62693895+eshilia@users.noreply.github.com>
2021-02-22 00:05:04 +0900

    Goal 기본헤더

--- Kirbies/01_WinMain/Goal.cpp
+++ Kirbies/01_WinMain/Goal.cpp
@@ -1,2 +1,25 @@
 #include "pch.h"
 #include "Goal.h"
+
+Goal::Goal(const string& name, float x, float y)
+	:GameObject(name)
+{
+	mX = x;
+	mY = y;
+}
+
+void Goal::Init()
+{
+}
+
+void Goal::Release()
+{
+}
+
+void Goal::Update()
+{
+}
+
+void Goal::Render(HDC hdc)
+{
+}
