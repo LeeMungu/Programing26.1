e338d68037cad1f2def723c98236eb8024421292
whara123 <64121533+whara123@users.noreply.github.com>
2021-02-21 22:17:07 +0900

    dig
    dig init세팅

--- Kirbies/01_WinMain/Dig.h
+++ Kirbies/01_WinMain/Dig.h
@@ -7,11 +7,16 @@ class DigPoint;
 
 class Dig :public State
 {
-	bool mIsStateStart = false;
 
 	Bottom* mBottom;
+	RECT mDigRect;
+	float mX;
+	float mY;
+	float mSizeX;
+	float mSizeY;
 
 public:
+	void Init()override;
 	void Update()override;
 	void Render(HDC hdc)override;
 };
