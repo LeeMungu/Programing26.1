910c00294a16951c615865a2b218c0cfde2c51c2
LeeMungu <jjjsd4715@naver.com>
2021-02-22 09:12:23 +0900

    [LMG] 플레이어 불값 추가
    CHANGE BOOL
    Gool BOOL

--- Kirbies/01_WinMain/Player.h
+++ Kirbies/01_WinMain/Player.h
@@ -33,6 +33,7 @@ class Player : public GameObject
 
 	Animation* mCurrentAnimation;
 
+	bool mIsGool;
 	bool mIsChange;
 	//오른쪽 왼쪽 구분하는 변수 0이면 오른쪽 1이면 왼쪽
 	int mIsMotionRL;
@@ -66,5 +67,11 @@ public:
 
 	PlayerState GetPlayerState() const { return mPlayerState; }
 	void SetPlayerState(PlayerState playerState) { mPlayerState = playerState; }
+
+	bool GetIsGool()const { return mIsGool; }
+	void SetIsGool(bool isGool) { mIsGool = isGool; }
+
+	bool GetIsChange()const { return mIsGool; }
+	void SetIsChange(bool isChange) { mIsGool = isChange; }
 };
 
