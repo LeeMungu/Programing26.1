cc641ec343493e5c802a3e507c447cb0b732088d
yuchan choe <youcank20@gmail.com>
2021-02-23 11:07:54 +0900

    카운트 유아이 대충

---
+++ Kirbies/01_WinMain/CountingPlayerUI.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "GameObject.h"
+
+class CountingPlayerUI : public GameObject 
+{
+	class Image* mImage;
+	class Door* mDoor;
+	float mCountTimer;
+	float mTimer;
+	int mPlayerCount;
+	int mCreatedPlayerCount;
+	int mGoalPlayerCount;
+
+public:
+	CountingPlayerUI(float x, float y, float timer);
+
+	void Init()override;
+	void Release()override;
+	void Update()override;
+	void Render(HDC hdc)override;
+};
+
