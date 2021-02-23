357f11187dd9c7e80d115d293f20675ff37f4cbb
yuchan choe <youcank20@gmail.com>
2021-02-23 12:33:45 +0900

    고침

--- Kirbies/01_WinMain/Scene1.cpp
+++ Kirbies/01_WinMain/Scene1.cpp
@@ -32,15 +32,15 @@ void Scene1::Init()
 	Goal* goal = new Goal("goal", WINSIZEX / 2+300,200);
 	ObjectManager::GetInstance()->AddObject(ObjectLayer::Goal, goal);
 
-	Ui* ui = new Ui(PlayerState::BoomState, 100, 100, 20);
+	Ui* ui = new Ui("BoomBtn", PlayerState::BoomState, 100, 100, 20);
 	ObjectManager::GetInstance()->AddObject(ObjectLayer::UI, ui);
-	Ui* ui2 = new Ui(PlayerState::ClimbState, 100, 200, 20);
+	Ui* ui2 = new Ui("ClimbBtn", PlayerState::ClimbState, 100, 200, 20);
 	ObjectManager::GetInstance()->AddObject(ObjectLayer::UI, ui2);
-	Ui* ui3 = new Ui(PlayerState::DigState, 100, 300, 20);
+	Ui* ui3 = new Ui("DigBtn", PlayerState::DigState, 100, 300, 20);
 	ObjectManager::GetInstance()->AddObject(ObjectLayer::UI, ui3);
-	Ui* ui4 = new Ui(PlayerState::StopperState, 100, 400, 20);
+	Ui* ui4 = new Ui("StopperBtn", PlayerState::StopperState, 100, 400, 20);
 	ObjectManager::GetInstance()->AddObject(ObjectLayer::UI, ui4);
-	Ui* ui5 = new Ui(PlayerState::UmbrellaState, 100, 500, 20);
+	Ui* ui5 = new Ui("UmbrellaBtn", PlayerState::UmbrellaState, 100, 500, 20);
 	ObjectManager::GetInstance()->AddObject(ObjectLayer::UI, ui5);
 
 	//camera
