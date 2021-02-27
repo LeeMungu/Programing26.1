4da19716e196b17f98489098243d26f78b18d47a
LeeMungu <jjjsd4715@naver.com>
2021-02-27 20:26:02 +0900

    [LMG]~~

--- Kirbies/01_WinMain/SceneEnding.h
+++ Kirbies/01_WinMain/SceneEnding.h
@@ -1,5 +1,13 @@
 #pragma once
-class SceneEnding
+#include "Scene.h"
+
+class Image;
+class SceneEnding : public Scene
 {
+	Image* mImage;
+public:
+	void Init() override;
+	void Update() override;
+	void Render(HDC hdc) override;
 };
 
