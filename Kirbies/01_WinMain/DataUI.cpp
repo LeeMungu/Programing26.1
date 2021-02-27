c1d4d1aa120afd714b31887597fbe9e3b43053b2
yuchan choe <youcank20@gmail.com>
2021-02-27 18:01:29 +0900

    냠

--- Kirbies/01_WinMain/DataUI.cpp
+++ Kirbies/01_WinMain/DataUI.cpp
@@ -44,22 +44,33 @@ void DataUI::Update()
 
 void DataUI::Render(HDC hdc)
 {
-
 	ConfigUi* temp = (ConfigUi*)UiManager::GetInstance()->FindUi("Menu");
-	
+
+	SetBkMode(hdc, 1);
+	HFONT hFont, oldFont;
+	hFont = CreateFont(15, 0, 0, 0, 0, 0, 0, 0, HANGUL_CHARSET, 0, 0, 0, VARIABLE_PITCH || FF_ROMAN, TEXT("Edit Undo BRK"));
+	oldFont = (HFONT)SelectObject(hdc, hFont);
+
+	wstring str = L"STAGE   IN   TIME";
+	TextOut(hdc, WINSIZEX / 2 + 150, WINSIZEY / 2 - 100, str.c_str(), str.length());
+
 	if (temp->GetIsRecordMenu() == true)
 	{
 
 		if (mGoalPercentList.size() != NULL) {
 			for (int i = 0; i < tempCountingUI.size(); i++) {
+				wstring stage = to_wstring(i + 1);
 				wstring goal = to_wstring((int)mGoalPercentList[i]) + L"%";
 				wstring timer = to_wstring((int)(mTimeList[i] / 60) % 60) + L":" + to_wstring((int)mTimeList[i] % 60);
-
-				TextOut(hdc, WINSIZEX / 2, WINSIZEY / 2 + 50 * i, goal.c_str(), goal.length());
-				TextOut(hdc, WINSIZEX / 2 + 100, WINSIZEY / 2 + 50 * i, timer.c_str(), timer.length());
+				TextOut(hdc, WINSIZEX / 2 + 170, WINSIZEY / 2 - 80 + 50 * i, stage.c_str(), stage.length());
+				TextOut(hdc, WINSIZEX / 2 + 220, WINSIZEY / 2 - 80 + 50 * i, goal.c_str(), goal.length());
+				TextOut(hdc, WINSIZEX / 2 + 270, WINSIZEY / 2 - 80 + 50 * i, timer.c_str(), timer.length());
 			}
 		}
 	}
+
+	SelectObject(hdc, oldFont);
+	DeleteObject(hFont);
 
 	//wstring GoalPlayer = to_wstring((int)mGoalPercent);
 	//wstring timer = to_wstring((int)(mTime / 60) % 60) + L":"
