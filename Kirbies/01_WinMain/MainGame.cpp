7edb8b882f9fcdee38523186ed570b732576a9ed
LeeMungu <jjjsd4715@naver.com>
2021-02-28 17:13:29 +0900

    로딩씬 업댓

--- Kirbies/01_WinMain/MainGame.cpp
+++ Kirbies/01_WinMain/MainGame.cpp
@@ -72,7 +72,7 @@ void MainGame::Init()
 	SceneManager::GetInstance()->AddScene(L"LoadingScene4to5", new LoadingScene4to5);
 	
 	//처음은 메인화면
-	SceneManager::GetInstance()->LoadScene(L"LoadingScene4to5");
+	SceneManager::GetInstance()->LoadScene(L"MainScene");
 
 	//디디디
 	IMAGEMANAGER->LoadFromFile(L"dedede", Resources(L"dedede.bmp"), 256, 244, 4, 4, true);
